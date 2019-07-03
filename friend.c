5ac3c9765e82987753a7818d561389f3a6a9e9ec
Jatin Watts <51354333+jatinwatts007@users.noreply.github.com>
2019-07-03 22:59:41 +0530

    Create friend.c

---
+++ friend.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<conio.h>
+#include<iostream.h>
+class A
+{
+	private:
+	int a;
+	int b;
+	public:
+	void fun(int x, int y)
+	{
+		a=x*y;
+		b=x+y;
+	}
+	void show()
+	{
+		cout<<a;
+		cout<<b;
+	}
+};
+class B
+{
+
+	friend void class A::fun(int x,int y);
+	friend void class A::show();
+};
+void main()
+{
+	B b1;
+	b1.fun();
+	b1.show();
+	getch();
+}
