fe9f32435afca6ab72798cc399e4ff04b53a7284
Jatin Watts <51354333+jatinwatts007@users.noreply.github.com>
2019-08-07 22:31:07 +0530

    Create sum of divisiors of divisiors.cpp

---
+++ sum of divisiors of divisiors.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+int main()
+ {
+     int i,k,l,h,n;
+     cin>>l;
+     while(l--)
+     {
+         i=0;
+         cin>>h;
+         k=h;
+         while(k>0)
+         {
+             if(h%k==0)
+             {
+                 n=k;
+             }
+             while(n>0)
+             {
+                 if(k%n==0)
+                 {
+                     i=i+n;
+                 }
+                 n--;
+             }
+             k--;
+         }
+         cout<<i<<endl;
+     }
+	//code
+	return 0;
+}
