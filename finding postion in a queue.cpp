74c02f0f0eacfcd0e41ab68ad579465de5c522db
Jatin Watts <51354333+jatinwatts007@users.noreply.github.com>
2019-08-16 22:17:04 +0530

    Create finding postion in a queue.cpp

---
+++ finding postion in a queue.cpp
@@ -0,0 +1,16 @@
+ {
+     int i,j,k,t,m,n;
+     cin>>t;
+     while(t--)
+     {
+         m=2;
+         cin>>n;
+         while(n/m>0)
+         {
+             m=m*2;
+         }
+         cout<<m/2<<endl;
+     }
+	//code
+	return 0;
+}
