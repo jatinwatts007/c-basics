26fb6448fb056ca13267d2effcabea9c695937a5
Jatin Watts <51354333+jatinwatts007@users.noreply.github.com>
2019-08-17 22:25:55 +0530

    Create Check if a number is Perfect Square.cpp

---
+++ Check if a number is Perfect Square.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+ {
+	int t;
+	cin>>t;
+	while(t--){
+	    int n;
+	    cin>>n;
+	    float m=pow(n,0.5);
+	    int a=ceil(m);
+	    m=m*a;
+	    if(n==m){
+	        cout<<"1"<<endl;
+	    }
+	    else{
+	        cout<<"0"<<endl;
+	    }
+	    
+	}
+	return 0;
+}
