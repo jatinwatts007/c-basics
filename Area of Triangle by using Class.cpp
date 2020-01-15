208e9ca5b9ff2360ba37030dcc7cc66569f820f8
Jatin Watts <51354333+jatinwatts007@users.noreply.github.com>
2020-01-15 21:15:53 +0530

    Create Area of Triangle by using Class.cpp

---
+++ Area of Triangle by using Class.cpp
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<conio.h>
+#include<iostream.h>
+using namespace std;
+class area
+{
+	private:
+	int length,breath,area;
+	public:
+	void calculation(int i,int j)
+	{
+		area=(i*j)/2;
+	}
+	void show_data()
+	{
+		cout<<"area of triangle="<<area;
+	}
+};
+void main()
+{
+	clrscr();
+	area a1;
+	int r,f;
+	cout<<"enter length and breath to see the area of triangle\nlength=";
+	cin>>r;
+	cout<<"breath=";
+	cin>>f;
+	a1.calculation(r,f);
+	a1.show_data();
+	getch();
+}
