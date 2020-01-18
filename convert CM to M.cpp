fe45c61e66724a31b7a27a93f14c635c78092153
Jatin Watts <51354333+jatinwatts007@users.noreply.github.com>
2020-01-18 21:27:25 +0530

    Create convert CM to M.cpp

---
+++ convert CM to M.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	float a,c;
+	clrscr();
+	printf("enter any value to convert it into centimetre to metre\na=");
+	scanf("%f",&a);
+	c=a/100;
+	printf("c=%f",c);
+	getch();
+}
