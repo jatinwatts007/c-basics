a780dd8de9af0717100c6533e01ec0f5620567e7
Jatin Watts <51354333+jatinwatts007@users.noreply.github.com>
2020-03-01 22:55:59 +0530

    Create Inline function.cpp

---
+++ Inline function.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+inline void sum()  //definition
+{
+	int x,y;
+	cout<<"enter x and y:";
+	cin>>x>>y;
+	cout<<"addition is :"<<x+y;
+}
+main()
+{
+sum();   //call
+}
