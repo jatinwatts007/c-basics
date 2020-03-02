3d8343c57bfd9a30dc2419d91ca60841bc4ce1cd
Jatin Watts <51354333+jatinwatts007@users.noreply.github.com>
2020-03-02 22:20:53 +0530

    Create call by value.cpp

---
+++ call by value.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+display(int x)  //function definition
+{
+	x=x+1;
+	cout<<"value of x in display is: "<<x<<endl;
+	cout<<"address of x in display is: "<<&x<<endl;
+}
+main()
+{
+	int x;
+	cout<<"enter x:";
+	cin>>x;
+	display(x);  //function call
+	cout<<"value of x in main is: "<<x<<endl;
+	cout<<"address of x in main is "<<&x<<endl;
+}
